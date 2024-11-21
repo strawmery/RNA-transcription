8e785c3387ddfea8c81c728444e50deb0528a2d8
Maria Garcia <mgarciasierragrsr@gmail.com>
2024-11-21 10:44:59 +0100

    "Implemented RNA transcription functionality in Java, updated main method to test transcription, and enabled unit tests."

--- Kata-RnaTranscription-Java-main/src/main/java/org/factoriaf5/rna_transcription/RnaTranscription.java
+++ Kata-RnaTranscription-Java-main/src/main/java/org/factoriaf5/rna_transcription/RnaTranscription.java
@@ -3,8 +3,22 @@ package org.factoriaf5.rna_transcription;
 
 public class RnaTranscription {
     
-    String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Implement method");
+    static String transcribe(String dnaStrand) {
+        StringBuilder rnaStrand = new StringBuilder();
+        for(int i = 0; i < dnaStrand.length(); i++) {
+            char nucleotide = dnaStrand.charAt(i);
+            switch(nucleotide){
+                case 'A': rnaStrand.append('U'); 
+                        break;
+                case 'T': rnaStrand.append('A');
+                        break;
+                case 'C': rnaStrand.append('G');
+                        break;
+                case 'G': rnaStrand.append('C');
+                        break;
+            }
+        }
+            return rnaStrand.toString();
     }
 
 }
--- Kata-RnaTranscription-Java-main/src/test/java/org/factoriaf5/rna_transcription/RnaTranscriptionTest.java
+++ Kata-RnaTranscription-Java-main/src/test/java/org/factoriaf5/rna_transcription/RnaTranscriptionTest.java
@@ -3,9 +3,7 @@ package org.factoriaf5.rna_transcription;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
-
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class RnaTranscriptionTest {
@@ -22,31 +20,26 @@ public class RnaTranscriptionTest {
         assertThat(rnaTranscription.transcribe("").isEmpty(), is(true));
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testRnaTranscriptionOfCytosineIsGuanine() {
         assertThat(rnaTranscription.transcribe("C"), is(equalTo("G")));
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testRnaTranscriptionOfGuanineIsCytosine() {
         assertThat(rnaTranscription.transcribe("G"), is(equalTo("C")));
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testRnaTranscriptionOfThymineIsAdenine() {
         assertThat(rnaTranscription.transcribe("T"), is(equalTo("A")));
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testRnaTranscriptionOfAdenineIsUracil() {
         assertThat(rnaTranscription.transcribe("A"), is(equalTo("U")));
     }
 
-    @Disabled("Remove to run test")
     @Test
     public void testRnaTranscription() {
         assertThat(rnaTranscription.transcribe("ACGTGGTCTTAA"), is(equalTo("UGCACCAGAAUU")));
